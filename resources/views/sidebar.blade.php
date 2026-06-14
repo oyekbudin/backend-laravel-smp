5c1a468fa734fe8c6a10e7e94c7ff985c0ae0dad
oyekbudin <oyekbudin@gmail.com>
2026-06-14 11:50:07 +0700

    home

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -76,6 +76,16 @@
                         <span class="nav-link-text ms-1">Kalender Pendidikan</span>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::is('plang*') ? 'active' : '' }}" href="/plang">
+                        <div
+                            class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="bi bi-building text-dark text-sm opacity-10"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Papan Nama</span>
+                    </a>
+                </li>
                 <!--li class="nav-item">
         <a class="nav-link {{ Request::is('modestandby') ? 'active' : '' }}" href="/modestandby">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
