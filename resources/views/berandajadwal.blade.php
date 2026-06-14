c589ab69975c666354009fbeb8bd44f40c478931
oyekbudin <oyekbudin@gmail.com>
2026-06-15 05:23:10 +0700

    jadwal

--- resources/views/berandajadwal.blade.php
+++ resources/views/berandajadwal.blade.php
@@ -71,9 +71,9 @@
 
 
 
-                            <div class="flex-grow-1">
+                            
 
-                                <div class="d-flex justify-content-between align-items-center mb-2">
+                                <div class="d-flex justify-content-between align-items-center mb-2 w-100">
                                     <div class="d-flex justify-content-start gap-3 align-items-center ">
                                         <div class="rounded-circle d-flex align-items-center justify-content-center"
                                             style="width:30px; height:30px; background: {{ $guru->kode_warna }};">
@@ -148,7 +148,7 @@
 
                                 </div>
 
-                            </div>
+                            
 
                         </div>
                     @empty
